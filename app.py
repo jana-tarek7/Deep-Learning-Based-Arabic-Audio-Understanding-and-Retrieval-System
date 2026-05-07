7caa83462684ad4aba3420fa76f588d4a4f08d49
Jana Tarek <janatarek731@gmail.com>
2026-05-07 20:49:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,20 @@
 import streamlit as st
-from src.speech_to_text import transcribe_audio
-from src.summarization import summarize_text
-from src.search import search_query
+from src.asr import transcribe
+from src.summarization import summarize
 
-st.title("🎧 Audio AI Project")
+st.title("Arabic Audio Understanding System")
 
-audio = st.file_uploader("Upload Audio File", type=["wav", "mp3"])
+audio_file = st.file_uploader("Upload audio file")
 
-if audio:
-    st.audio(audio)
+if audio_file:
+    with open("temp.wav", "wb") as f:
+        f.write(audio_file.read())
 
-    st.write("🔄 Transcribing...")
-    text = transcribe_audio(audio)
+    text = transcribe("temp.wav")
+    summary = summarize(text)
 
     st.subheader("Transcript")
     st.write(text)
 
-    st.write("🧠 Summarizing...")
-    summary = summarize_text(text)
-
     st.subheader("Summary")
     st.write(summary)
-
-    query = st.text_input("Search in audio")
-
-    if query:
-        results = search_query(query)
-        st.write(results)
