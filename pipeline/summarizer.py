1d47d55ce9fa55ca715ad8347f33c0ea571b744b
Jana Tarek <janatarek731@gmail.com>
2026-05-07 21:10:28 +0300

    Refactor summarization to use AraT5v2 model
    Refactor summarization logic to use a specific model and tokenizer. Update function to include input formatting and device handling.

--- pipeline/summarizer.py
+++ pipeline/summarizer.py
@@ -1,6 +1,25 @@
-from transformers import pipeline
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-summarizer = pipeline("summarization")
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def summarize_text(text):
-    return summarizer(text, max_length=150, min_length=30, do_sample=False)[0]['summary_text']
+model_name = "omarsabri8756/AraT5v2-XLSum-arabic-text-summarization"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
+model.eval()
+
+def summarize(text):
+    text = "أعد صياغة النص التالي بشكل خبري مختصر: " + text
+
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, max_length=512)
+    inputs = {k: v.to(device) for k, v in inputs.items()}
+
+    with torch.no_grad():
+        output = model.generate(
+            **inputs,
+            max_length=120,
+            num_beams=6
+        )
+
+    return tokenizer.decode(output[0], skip_special_tokens=True)
