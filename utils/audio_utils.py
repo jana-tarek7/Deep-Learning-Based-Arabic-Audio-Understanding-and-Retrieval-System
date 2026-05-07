b886bb83c6a1772f857a3b55c49f91192601639b
Jana Tarek <janatarek731@gmail.com>
2026-05-07 21:10:07 +0300

    Add audio loading and resampling function

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -1,1 +1,12 @@
+import torchaudio
 
+def load_audio(path, target_sr=16000):
+    wav, sr = torchaudio.load(path)
+
+    if wav.shape[0] > 1:
+        wav = wav.mean(dim=0, keepdim=True)
+
+    if sr != target_sr:
+        wav = torchaudio.functional.resample(wav, sr, target_sr)
+
+    return wav.squeeze(0)
